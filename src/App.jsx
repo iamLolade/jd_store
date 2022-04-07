726962c86741327267f263660d0420b5710373d4
iamLolade <adeniyiotemade@gmail.com>
2022-04-07 01:18:51 +0100

    Updated file structure | Created HomePage and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home"
 
 function App() {
   return (
     <div className="App">
-      <h1>JD Store</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import "../Styles/Home/HomePage.css"
+
+class Home extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div className="layout">
+                <h3>Welcome</h3>
+            </div>
+        );
+    }
+}
+ 
+export default Home;
